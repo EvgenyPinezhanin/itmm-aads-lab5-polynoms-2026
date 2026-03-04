01ce546dd4c70c2db1a5e21f5c29614892d2b31e
EvgenyPinezhanin <pinezhanin@yandex.ru>
2026-03-04 23:40:42 +0300

    Added polynom template and sample

---
+++ samples/sample_polynom.cpp
@@ -0,0 +1,24 @@
+﻿// ННГУ, ИИТММ, Курс "Алгоритмы и структуры данных"
+//
+// Copyright (c) Пинежанин Е.С.
+//
+// Тестирование работы с полиномами
+
+#include <iostream>
+
+#include "tpolynom.h"
+
+int main()
+{
+  TPolynom p1;
+  p1.SetPolynom("3x^2y^4 + 4.5yz^2");
+
+  TPolynom p2;
+  p1.SetPolynom("3x^2y^4 + 4.5yz^2");
+
+  TPolynom p3 = p1 + p2;
+
+  std::cout << p3 << " = " << p3.Calculate(1.0, 2.5, 4.7) << "\n";
+
+  return 0;
+}
---
+++ src/tpolynom.cpp
@@ -0,0 +1,53 @@
+// ННГУ, ИИТММ, Курс "Алгоритмы и структуры данных"
+//
+// tcomputecentermodel.cpp
+//
+// Copyright (c) Пинежанин Е.С.
+
+#include <tpolynom.h>
+
+TPolynom::TPolynom() {};
+
+bool TPolynom::IsCorrect() const {
+
+}
+
+void TPolynom::SetPolynom(const std::string& polynom) {
+
+}
+
+TPolynom& TPolynom::operator+(const TPolynom &polynom) const {
+
+}
+
+TPolynom& TPolynom::operator-(const TPolynom &polynom) const {
+
+}
+
+TPolynom& TPolynom::operator*(const TPolynom &polynom) const {
+
+}
+
+TPolynom& TPolynom::operator*(double coeff) const {
+
+}
+
+void TPolynom::Add(const std::string& monom) const {
+
+}
+
+void TPolynom::Delete(size_t pos) {
+
+}
+
+double TPolynom::Calculate(double x, double y, double z) const {
+
+}
+
+std::istream& operator>>(std::istream& istr, TPolynom& polynom) {
+
+}
+
+std::ostream& operator<<(std::ostream& ostr, const TPolynom& polynom) {
+
+}
